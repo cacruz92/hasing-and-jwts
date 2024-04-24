d83bd949efde1c2ba9df9df549881f58aa7ce2c4
Christian Cruz <christian.cruz92@hotmail.com>
2024-04-24 11:09:37 -0400

    added register and authenticate methods to the user model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 /** User class for message.ly */
-
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const ExpressError = require("../expressError");
+const { BCRYPT_WORK_FACTOR, SECRET_KEY } = require("../config")
 
 
 /** User of the site. */
@@ -10,11 +13,45 @@ class User {
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+  static async register({username, password, first_name, last_name, phone}) {
+    try{
+      if(!username || !password){
+        throw new ExpressError("Username and password required", 400);
+      }
+  
+      let hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+  
+      const results = await db.query(`INSERT INTO users(username, password, first_name, last_name, phone, join_at, last_login_at)
+      VALUES($1, $2, $3, $4, $5, current_timestamp, current_timestamp)
+      RETURNING username, first_name, last_name, phone`,
+    [username, hashedPassword, first_name, last_name, phone]);
+    returne res.json(results.rows[0]);
+    } catch(e) {
+      next(e);
+    }
+
+
+   }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { }
+  static async authenticate(username, password) {
+    try{
+      if(!username || !password){
+        throw new ExpressError("Username and password required", 400);
+      }
+      const results = await db.query(`SELECT username, password FROM users WHERE username = $1`,[username]);
+      const user = results.rows[0];
+      if(user){
+        if(await bcrypt.compare(password, user.password)){
+          const token = jwt.sign({ username }, SECRET_KEY);
+          return res.json({message: `Logged in!`, token})
+        }
+      }
+    } catch(e){
+      next(e)
+    }
+  }
 
   /** Update last_login_at for user */
 
